2864f89ec3a6810778dbf49809c15af0bea3afeb
Tasmia Quadir <tasmia.quadir@gmail.com>
2023-02-06 20:49:43 -0500

    commit tree 8d682eaea703d393d8f6c4361da010759b66ed19

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(hip = "roller-skate"){
+    return (`This Saturday, I want to ${hip}!`)
+     }
+saturdayFun();
+saturdayFun("bathe my dog")
+
+  function mondayWork(kip = "go to the office"){
+    return (`This Monday, I will ${kip}.`)
+ };
+  mondayWork("work from home");
+
+  function wrapAdjective(str = "*") {
+    return function(pip = "special") {
+      return `You are ${str}${pip}${str}!`
+    }
+  }
+const innerFunction = wrapAdjective()
+innerFunction()
